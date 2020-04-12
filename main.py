07d961c50cc49c38621f447f563b49e72bcb8348
cadarsh <cadarsh335@gmail.com>
2020-04-12 14:18:33 +0530

    final-1

--- main.py
+++ main.py
@@ -10,14 +10,16 @@ mainDir = 'C:\Projects\openCV\pdfdarkmode\darkmodepdf\pdf'
 
 
 def convertPDF():
-    images = convert_from_path(mainDir+'\sample.pdf')
+    images = convert_from_path(mainDir+'\sample1.pdf')
     for i,image in enumerate(images):
         image = np.array(image)
         beforeImg = image
         image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
         _,image = cv2.threshold(image,60,255,cv2.THRESH_BINARY_INV)
-        image = cv2.blur(image,(2,2))
-        images[i] = i;
+        blur = cv2.GaussianBlur(image,(3,3),0)
+        imgnew = cv2.addWeighted(blur,2,image,-0.5,0)
+        kernel = np.ones((1,1),np.uint8)
+        image = cv2.dilate(imgnew,kernel,iterations=1)
         cv2.imwrite(mainDir+'\image'+str(i)+'.png',image)
         print("Lol")
      
@@ -40,7 +42,7 @@ def makePDF():
     for i in range(len(list_of_images)):
         images.append(Image.open(mainDir+'\\'+list_of_images[i]))
 
-    images[0].save(mainDir+'\\'+'converted.pdf',save_all=True,append_images = images[1:])
+    images[0].save(mainDir+'\\'+'converted1.pdf',save_all=True,append_images = images[1:])
     print(images)
 
 def delImages():
